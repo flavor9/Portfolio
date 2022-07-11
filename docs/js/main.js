53bc692c6a0150e305017f460474cb0765b54f23
Vsevolod Railean <sevarailean61@gmail.com>
2022-07-11 21:41:42 +0300

    day 3

--- docs/js/main.js
+++ docs/js/main.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () =>{
 closeElem.addEventListener('click', () =>{
 	menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.skills__percentage-percent'),
+	  lines = document.querySelectorAll('.skills__percentage-fullbar span');
+	  
+	  counters.forEach( (item, i) => {
+		lines[i].style.width = item.innerHTML;
+	});
